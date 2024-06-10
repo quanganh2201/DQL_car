760a7a58f0d8bbd26d9573672ef5a7e7b7ebb8ef
quanganh2201 <vuquanganh2201@gmail.com>
2024-06-10 11:27:59 +0700

    test model

--- run.py
+++ run.py
@@ -1,24 +1,31 @@
 from env import Env
 import numpy as np
+import model5
+import torch
 env = Env() #khởi tạo môi trường
-EPISODES = 10 # số vòng chạy
-steps = 1000 # số bước chạy
+EPISODES = 5 # số vòng chạy
+steps = 500 # số bước chạy
+model_filepath = ...#tên file đã lưu
+score = 0
+num_states = 6
+num_actions = 3
+pre_action = None
+policy_dqn = model5.DeepQNetwork(input_dims=num_states, fc1_dims=64, fc2_dims=64,
+                                      n_actions=num_actions)
+policy_dqn.load_state_dict(torch.load(model_filepath))
+policy_dqn.eval()
 for e in range(EPISODES):
     done = False
     state = env.reset() # khởi tạo ngẫu nhiên giá trị state
-    score = 0
-    pre_action = None
-    for t in range(steps):
-        action = np.random.choice([0,1,2]) # chọn ngẫu nhiên action
-        reward = env.setReward(state, pre_action, action) # nhận reward
-        next_state, done = env.step(action) #chọn giá trị mới theo step
-        pre_action = action 
+    while not done:
+        with torch.no_grad():
+            action = policy_dqn(model5.state_to_dqn_input(state)).argmax().item()
+        reward, done = env.setReward(state, pre_action, action) # nhận reward
+        next_state = env.step(action) #chọn giá trị mới theo step
+        pre_action = action
         score = +reward
         state = next_state
-        if t%5 == 0:
-            print("episodes: %d; score %d; step: %d", e, score,t)
-        if t == 1000 or score <= -30 or score >= 30:
-            done = True
+        if done:
             print("episodes: %d done; total_score %d; total_step: %d", e, score,t)
             break
 quit()
